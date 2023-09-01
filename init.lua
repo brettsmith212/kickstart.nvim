6ed4589e419863c807d4fca8ebd4330816152e8a
Brett Smith <brettsmith212@gmail.com>
2023-08-31 21:47:31 -0700

    neotree toggle <leader>ft

--- init.lua
+++ init.lua
@@ -264,6 +264,9 @@ vim.o.termguicolors = true
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- Neotree
+vim.keymap.set("n", "<leader>ft", ":Neotree toggle<CR>")
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
